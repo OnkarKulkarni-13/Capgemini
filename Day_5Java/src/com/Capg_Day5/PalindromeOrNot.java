c4af406189417a195127a57c7fcd3b17e2c5d239
OnkarKulkarni-13 <98728051+OnkarKulkarni-13@users.noreply.github.com>
2022-02-02 19:12:59 +0530

    first commit

---
+++ Day_5Java/src/com/Capg_Day5/PalindromeOrNot.java
@@ -0,0 +1,21 @@
+package com.Capg_Day5;
+import java.util.Scanner;
+
+public class PalindromeOrNot {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Enter your string:- ");
+		String str =  sc.nextLine();
+		
+		//StringBuffer str1 = new StringBuffer(str);  ---->>>>>>
+		//str1.reverse();
+		
+		/*if(str.compareTo(str1))              ------->>>>> this is not valid we can't compare String with StringBuffer
+		 *  {
+			System.out.println("Palindrome String");
+			
+		}*/
+		
+	}
+
+}
